f16cdb34348f6360cb0826fe55ea212a4d60be62
TreRote42 <pietrimonta8@gmail.com>
2023-05-26 20:29:44 +0200

    Aggiunte classi accelerometro

---
+++ app/src/main/java/com/example/cameraheartbeat/myInterface/IMyAccelerometer.java
@@ -0,0 +1,4 @@
+package com.example.cameraheartbeat.myInterface;
+
+public interface IMyAccelerometer {
+}
---
+++ app/src/main/java/com/example/cameraheartbeat/utilities/MyAccelerometer.java
@@ -0,0 +1,4 @@
+package com.example.cameraheartbeat.utilities;
+
+public class MyAccelerometer {
+}
